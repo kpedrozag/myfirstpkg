554b293a9b1c555350cdea2861da09b8d54dbd08
Kevin pedroza <kevin.andres01@hotmail.com>
2018-11-18 14:32:17 -0500

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='indexor',
-    version='0.1.0',
+    version='0.1.1',
     description='The best indexor',
     url='https://github.com/kpedrozag/myfirstpkg',
     author='Kevin Pedroza Goenaga',
